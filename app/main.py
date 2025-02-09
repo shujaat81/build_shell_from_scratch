c0fe52cc3eeed5e952d434226ddfb5a56c7fcb08
shujaat <hasanshujaat4@gmail.com>
2025-02-09 20:57:45 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -3,20 +3,20 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    exit_message = 'exit 0'
     while True:
         sys.stdout.write("$ ")
 
         # Wait for user input
-        command = input()
-        if command == exit_message:
-            sys.exit(0)
-        if len(command) != 0:
-            first_input = command.split()[0]
-            if first_input == "echo":
-                print(f"{command[len(first_input)+1:]}")
-            else:
-                print(f"{command}: command not found")
+        command, *args = input().split(" ")
+
+        match command:
+            case "exit":
+                break
+            case "echo":
+                print("".join(args))
+            case default:
+                sys.stdout.write(f"{command}: command not found\n")
+        
         
 
 
