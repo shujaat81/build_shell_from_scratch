221d77d07005b1e24dcd531fa36216d45668e826
shujaat <hasanshujaat4@gmail.com>
2025-02-09 20:17:53 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -4,16 +4,14 @@ import sys
 def main():
     # Uncomment this block to pass the first stage
     exit_message = 'exit 0'
-    loop = True
-    while loop:
+    while True:
         sys.stdout.write("$ ")
 
         # Wait for user input
         command = input()
-        print(f"{command}: command not found")
         if command == exit_message:
-            loop = False
             sys.exit(0)
+        print(f"{command}: command not found")
         
 
 
