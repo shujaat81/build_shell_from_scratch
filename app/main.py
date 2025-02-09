dddd91e5029e826b6500ec342e29c3534c55b408
shujaat <hasanshujaat4@gmail.com>
2025-02-09 20:28:50 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def main():
         command = input()
         if command == exit_message:
             sys.exit(0)
-        print(f"\n{command}")
+        print(f"\n{str(command)}")
         
 
 
