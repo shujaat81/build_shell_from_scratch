371b768893c563e9163d8dcf03d61e3ce20319b6
shujaat <hasanshujaat4@gmail.com>
2025-02-09 20:59:06 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,24 +1,17 @@
 import sys
-
-
 def main():
-    # Uncomment this block to pass the first stage
     while True:
+        # Uncomment this block to pass the first stage
         sys.stdout.write("$ ")
-
         # Wait for user input
         command, *args = input().split(" ")
-
         match command:
             case "exit":
                 break
             case "echo":
-                print("".join(args))
+                print(" ".join(args))
             case default:
                 sys.stdout.write(f"{command}: command not found\n")
-        
-        
-
-
+    return
 if __name__ == "__main__":
     main()
